60bac5eae519c3cac5ce8a930339c753fda4e009
Tristan <rechenberger@me.com>
2018-02-23 21:26:40 +0100

    material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
+import { MaterialModule } from './material.module';
 import { DataService } from './services/data.service';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
@@ -19,7 +21,9 @@ import { AppSwitcherComponent } from './components/app-switcher/app-switcher.com
     BrowserModule,
     AppRoutingModule,
     AngularFireModule,
-    FormsModule
+    FormsModule,
+    MaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [
     DataService
---
+++ src/app/material.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule, MatCardModule, MatProgressBarModule, MatIconModule, MatMenuModule, MatToolbarModule, MatFormFieldModule, MatInputModule, MatExpansionModule, MatCheckboxModule, MatTooltipModule, MatListModule, MatSnackBarModule, MatChipsModule, MatProgressSpinnerModule, MatDialogModule, MatTabsModule, MatButtonToggle, MatButtonToggleModule } from '@angular/material';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
+
+const modules = [
+  MatButtonModule,
+  MatCardModule,
+  MatProgressBarModule,
+  MatIconModule,
+  MatMenuModule,
+  MatToolbarModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatExpansionModule,
+  MatCheckboxModule,
+  MatTooltipModule,
+  MatListModule,
+  MatSelectModule,
+  MatTableModule,
+  MatSnackBarModule,
+  MatChipsModule,
+  MatProgressSpinnerModule,
+  MatDialogModule,
+  MatTabsModule,
+  MatButtonToggleModule
+]
+
+@NgModule({
+  imports: modules,
+  exports: modules,
+  declarations: []
+})
+export class MaterialModule { }
