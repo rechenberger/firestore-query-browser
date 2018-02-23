0dd9e0bb33d8e2263758c49505b33bc8b92d9e9e
Tristan <rechenberger@me.com>
2018-02-23 21:07:06 +0100

    switcher parse

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { DataService } from './services/data.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AngularFireModule } from 'angularfire2';
@@ -17,7 +18,8 @@ import { AppSwitcherComponent } from './components/app-switcher/app-switcher.com
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AngularFireModule
+    AngularFireModule,
+    FormsModule
   ],
   providers: [
     DataService
--- src/app/components/app-switcher/app-switcher.component.ts
+++ src/app/components/app-switcher/app-switcher.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppSwitcherComponent implements OnInit {
 
+  configString = `{
+    apiKey: "AIzaSyBpFFZomGDJK-pzaN1g6IoglAF6YCxl43Q",
+    authDomain: "crowd-e.firebaseapp.com",
+    databaseURL: "https://crowd-e.firebaseio.com",
+    projectId: "crowd-e",
+    storageBucket: "crowd-e.appspot.com",
+    messagingSenderId: "920940631373"
+  }`
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  switch() {
+    const json = this.configString.replace(/([a-zA-Z]*)(: ")/g, `"$1"$2`)
+    const config = JSON.parse(json)
   }
 
 }
