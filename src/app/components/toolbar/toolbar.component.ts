fe754997b7f916dc7b0717461c1ab097ee2291fa
Tristan <rechenberger@me.com>
2018-04-16 14:52:54 +0200

    toolbar ngif fix

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { AppsService } from '../../services/apps.service'
 
 @Component({
   selector: 'app-toolbar',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToolbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public apps: AppsService
+  ) { }
 
   ngOnInit() {
   }
