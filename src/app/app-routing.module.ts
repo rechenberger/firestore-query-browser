76319381393ef7e5ae10775ce6426e18ef31bb43
Tristan <rechenberger@me.com>
2018-04-16 15:01:59 +0200

    login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,29 @@
-import { MainComponent } from './components/main/main.component';
-import { QueryBrowserComponent } from './components/query-browser/query-browser.component';
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './components/main/main.component'
+import { QueryBrowserComponent } from './components/query-browser/query-browser.component'
+import { NgModule } from '@angular/core'
+import { Routes, RouterModule } from '@angular/router'
+import { LoginComponent } from './components/login/login.component'
 
 const routes: Routes = [
   {
     path: '',
-    component: MainComponent
+    component: MainComponent,
+    children: [
+      {
+        path: '',
+        component: QueryBrowserComponent
+      },
+      {
+        path: 'login',
+        component: LoginComponent
+      }
+    ]
   },
   {
     path: '**',
     redirectTo: '/'
   }
-];
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
